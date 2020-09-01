9d228655f6cb8540e190a7628d6d08fbf045bebe
Ashleigh Whyte <ashleighwhyte@hotmail.com>
2020-09-01 20:39:19 +1000

    uploading password generator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Password-Generator
+# 03 JavaScript: Password Generator
+
+Random password generator
+
+Really struggled with this one as I am struggling to understand javascript so I had to do a lot of googling and youtubing. In the end I referenced a very similar code and 
+
---
+++ script.js
@@ -0,0 +1,84 @@
+// Assignment Code - Random Password Generator will create password between 8-128 based on variables
+var generateBtn = document.querySelector("#generate");
+
+//arrays list
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', '(', ')' ,'}' ,'{', '[', ']', '~', '-', '_', '.'];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//variables
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+//prompt to confirm how many characters for password
+function generatePassword() {
+  var confirmLength = (prompt("Select a number between 8-128 for your password length"));
+
+//loop if number selected is outside of 8-128
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-128 characters Try again");
+      var confirmLength = (prompt("Select a number between 8-128 for your password length"));
+      } 
+
+//confirmation for how many characters 
+      alert(`Your password will have ${confirmLength} characters`);
+
+//determine variables for password
+    var confirmSpecialCharacter = confirm("Click OK to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to include uppercase characters");
+
+//loop if variables not selected
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("You must choose at least one parameter");
+        var confirmSpecialCharacter = confirm("Click OK to include special characters");
+        var confirmNumericCharacter = confirm("Click OK to include numeric characters");    
+        var confirmLowerCase = confirm("Click OK to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to include uppercase characters"); 
+    } 
+
+      var passwordCharacters = []
+
+//generate random password - assign action based on variables
+    if (confirmSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    }
+
+    if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
+
+      console.log(passwordCharacters)
+
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
